ef24d3212f7464beb82ab1340940bddf4c8c145d
Sharathgowda96 <85106974+Sharathgowda96@users.noreply.github.com>
2023-08-02 22:10:48 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "username" {
+  type    = list(any)
+  default = ["terraform-jib", ]
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "instance_profile_name" {
+  type    = string
+  default = "example-instance-profile"
+}
+
+variable "iam_policy_name" {
+  type    = string
+  default = "example-policy"
+}
+
+variable "role_name" {
+  type    = string
+  default = "example-role"
+}
